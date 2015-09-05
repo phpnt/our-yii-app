39c03d75cdb7d4c9124e259d512a8e5c2858a2b1
phpnt <admin@phpnt.com>
2015-09-05 13:13:34 +0500

    Activation via email (false)

--- config/params.php
+++ config/params.php
@@ -4,5 +4,5 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'no-reply@phpnt.com',             // автоматическая отправка почты с данного емайл
     'secretKeyExpire' => 60 * 60,                       // время хранения секретного ключа
-    'emailActivation' => true,                          // активация по емайл
+    'emailActivation' => false,                         // активация по емайл
 ];
